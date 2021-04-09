c683b794a68e60f1cf93e68bef356d6f22c188a4
Christina Hawkins <christinahawkins@Christinas-MacBook-Pro.local>
2021-04-08 21:07:12 -0400

    svg

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,11 @@ var margin = 20;
 var label = 110;
 var tPadBot = 40;
 var tPadLeft = 40;
+
+
+var svg = d3
+.select("#scatter")
+.append("svg")
+.attr("width", width)
+.attr("height", height)
+.attr("class", "chart");
