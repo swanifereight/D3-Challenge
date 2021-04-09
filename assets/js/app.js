ff56f22d186549d94f43b0041523c6341e820cb0
Christina Hawkins <christinahawkins@Christinas-MacBook-Pro.local>
2021-04-08 21:02:35 -0400

    .

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,7 @@
 // @TODO: YOUR CODE HERE!
+var width = parseInt(d3.select("#scatter").style("width"));
+var height = width - width / 4.0;
+var margin = 20;
+var label = 110;
+var tPadBot = 40;
+var tPadLeft = 40;
