2e3db0e5033ec177ad6f6c5927c4ea6e381cd554
Christina Hawkins <christinahawkins@Christinas-MacBook-Pro.local>
2021-04-08 21:14:59 -0400

    text svg

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,28 @@ var svg = d3
 .attr("width", width)
 .attr("height", height)
 .attr("class", "chart");
+
+var circle;
+function crGet() {
+	if (width <= 530) {
+		circle = 5;
+	}
+	else{
+		circle = 10;
+	}
+}
+crGet();
+
+svg.append("g").attr("class", "xText");
+var xText = d3.select("xText");
+function xTextRefresh() {
+	xText.attr(
+		"transform",
+		"translate(" +
+		((width - label) / 2 + label) +
+		"," +
+		(height - margin - tPadBot) +
+		")"
+		);
+}
+xTextRefresh();
